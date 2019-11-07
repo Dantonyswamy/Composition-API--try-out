8d3e4cdac8fc493cd77dd22716ed4f7f403b3ed3
Dantony <dantonyswamy@realtermenergy.com>
2019-11-07 10:36:45 -0500

    Completed tryout
    Completed this try out by watching Erik's video tutorial.
    https://youtu.be/zPViRHZfKv4

--- src/components/SearchBrews.vue
+++ src/components/SearchBrews.vue
@@ -20,25 +20,37 @@
             </v-btn>
           </v-row>
         </v-form>
-        <v-list class="ma-2">
+        <v-list class="ma-2" v-for="brewery in list" :key="brewery.id">
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>Name :</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-content class="align-end">
+              {{ brewery.name }}
             </v-list-item-content>
           </v-list-item>
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>Street :</v-list-item-title>
             </v-list-item-content>
+            <v-list-item-content class="align-end">
+              {{ brewery.street }}
+            </v-list-item-content>
           </v-list-item>
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>City :</v-list-item-title>
             </v-list-item-content>
+            <v-list-item-content class="align-end">
+              {{ brewery.city }}
+            </v-list-item-content>
           </v-list-item>
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title>Zip :</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-content class="align-end">
+              {{ brewery.postal_code }}
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -47,21 +59,11 @@
   </v-container>
 </template>
 <script>
+import useBrewList from "../composable/brew-list-api";
 export default {
-  data() {
-    return {
-      val: "",
-      breweries: []
-    };
-  },
-  methods: {
-    async submitted() {
-      const response = await fetch(
-        `https://api.openbrewerydb.org/breweries/?by_name=${this.val}`
-      );
-      const json = await response.json();
-      console.log(json);
-    }
-  }
+  setup() {
+    const { val, list, submitted } = useBrewList();
+    return { val, list, submitted };
+  }  
 };
 </script>
